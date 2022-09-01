612d1316a2e4e9d425e4d24f443ae6c0ad2a55ea
Adam Kaminski <arturkars01@gmail.com>
2022-09-01 21:43:38 +0200

    Spring intilisation

---
+++ src/main/java/com/LPR/OpResearch/OpResearchApplication.java
@@ -0,0 +1,13 @@
+package com.LPR.OpResearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpResearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpResearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LPR/OpResearch/OpResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LPR.OpResearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpResearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
